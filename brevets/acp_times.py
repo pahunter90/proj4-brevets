a7025559540c0b44909c8f1d8c7576e1406296b7
Pierce Hunter <phunter@uoregon.edu>
2017-10-21 23:02:47 -0700

    Passes the correct times

--- brevets/acp_times.py
+++ brevets/acp_times.py
@@ -29,7 +29,16 @@ def open_time(control_dist_km, brevet_dist_km, brevet_start_time):
        An ISO 8601 format date string indicating the control open time.
        This will be in the same time zone as the brevet start time.
     """
-    return arrow.now().isoformat()
+    max_speeds = [(600,28),(400,30),(200,32),(0,34)]
+    time = 0
+    for i in range(len(max_speeds)):
+        distance, speed = max_speeds[i]
+        while control_dist_km > distance:
+            time += 60/speed
+            control_dist_km -= 1
+    time = round(time)
+    open_time = brevet_start_time.shift(minutes=+time)
+    return open_time.for_json()
 
 
 def close_time(control_dist_km, brevet_dist_km, brevet_start_time):
@@ -45,4 +54,23 @@ def close_time(control_dist_km, brevet_dist_km, brevet_start_time):
        An ISO 8601 format date string indicating the control close time.
        This will be in the same time zone as the brevet start time.
     """
-    return arrow.now().isoformat()
+    time = 0
+    max_times = [(200,13.5),(300,20),(400,27),(600,40),(1000,75)]
+    min_speed = [(600,11.428),(0,15)]
+    if control_dist_km == brevet_dist_km:
+        for i in range(len(max_times)):
+            dist, m_time = max_times[i]
+            if brevet_dist_km == dist:
+                time = m_time*60
+    else:
+        for j in range(len(min_speed)):
+            dist, speed = min_speed[j]
+            while control_dist_km > dist:
+                time += 60/speed
+                control_dist_km -= 1
+        time = round(time)
+        if time == 0:
+            time = 60
+    close_time = brevet_start_time.shift(minutes=+time)
+    return close_time.for_json()
+
